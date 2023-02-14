7c0b20e44803a3a173e7b448ddede1d541a1b490
Parth-kumar-1802 <parthkumar1802@gmail.com>
2023-02-14 20:36:22 +0530

    minor fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', require('./routes/user'));
 
-let port = 3001 || process.env.PORT;
+let port = 3000 || process.env.PORT;
 app.listen(port, (req, res) => {
     console.log(`The server is running on the port ${port}`);
 })
